8cd2b40f786b1929853e8045939a9dde91945f81
Aleksey Kuznetsov <oasisalex@gmail.com>
2025-04-09 01:29:56 +0300

    Scene, first car and URP settings

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField]
+    private Material mBaseMaterial = default;
+    [SerializeField]
+    private Color mColor = default;
+
+    private void OnValidate()
+    {
+        if (mBaseMaterial != null)
+        {
+            Debug.Log(mBaseMaterial.GetColor("baseColorFactor"));
+            mBaseMaterial.SetColor("baseColorFactor", mColor);
+        }
+    }
+}
---
+++ Assets/StandRotator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StandRotator : MonoBehaviour
+{
+    private Transform m_Transform;
+
+    [SerializeField]
+    private float m_RotateSpeed = 0.1f;
+
+    void Start()
+    {
+        m_Transform = GetComponent<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        m_Transform.Rotate(new Vector3(0, 1, 0), m_RotateSpeed);
+    }
+}
