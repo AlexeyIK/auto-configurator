fc3ef8bedbf77250c9a9e3076ba4df9567a20db8
akuznetsov <akuznetsov@vizex.ru>
2025-04-11 23:41:47 +0300

    Hover detector on tires and make freezing stand rotator after touch

---
+++ Assets/HoverDetector.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Material m_HoverMaterial = default;
+    [SerializeField] private MeshRenderer[] m_Meshes = default;
+    [SerializeField] private List<Material> m_DefaultMaterials = default;
+
+    private void Awake()
+    {
+        var meshCounter = 0;
+
+        foreach (var meshRenderer in m_Meshes)
+        {
+            m_DefaultMaterials.Add(meshRenderer.material);
+            meshCounter++;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        for (var i = 0; i < m_Meshes.Length; i++)
+            m_Meshes[i].material = m_HoverMaterial;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        for (var i = 0; i < m_Meshes.Length; i++)
+            m_Meshes[i].material = m_DefaultMaterials[i];
+    }
+}
--- Assets/StandRotator.cs
+++ Assets/StandRotator.cs
@@ -4,21 +4,32 @@ public class StandRotator : MonoBehaviour
 {
     private DragHandler m_DragHandler;
     private bool m_IsDragging;
+    private float m_RotateCountdown;
 
-    [SerializeField] private float m_RotateSpeed = 0.1f;
-    [SerializeField] private float m_DragRotateSensevity = 0.2f;
+    [SerializeField] private float m_RotateSpeed = -2f;
+    [SerializeField] private float m_DragRotateSensivity = 1f;
+    [SerializeField] private float m_RotatePause = 1f;
 
     private void Awake()
     {
         m_DragHandler = GetComponent<DragHandler>();
         m_DragHandler.DragStart += () => m_IsDragging = true;
-        m_DragHandler.DragEnd += () => m_IsDragging = false;
-        m_DragHandler.DragPerforming += (position, delta) => transform.Rotate(Vector3.up, -delta.x * m_DragRotateSensevity);
+        m_DragHandler.DragEnd += () =>
+        {
+            m_IsDragging = false;
+            m_RotateCountdown = m_RotatePause;
+        };
+        m_DragHandler.DragPerforming += (position, delta) => transform.Rotate(Vector3.up, -delta.x * m_DragRotateSensivity);
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         if (!m_IsDragging)
-            transform.Rotate(new Vector3(0, 1, 0), m_RotateSpeed);
+        {
+            if (m_RotateCountdown > 0)
+                m_RotateCountdown -= Time.deltaTime;
+            else
+                transform.Rotate(new Vector3(0, 1, 0), m_RotateSpeed * Time.deltaTime);
+        }
     }
 }
