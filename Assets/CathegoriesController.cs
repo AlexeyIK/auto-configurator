027a8ab9bb12a82bb8872f465bf8700704338161
akuznetsov <akuznetsov@vizex.ru>
2025-05-18 23:13:42 +0300

    items menu opener

--- Assets/CathegoriesController.cs
+++ Assets/CathegoriesController.cs
@@ -5,6 +5,8 @@ using UnityEngine.UIElements;
 public class CathegoriesController : MonoBehaviour
 {
     private UIDocument m_UIDocument;
+    private ListView m_CathegoriesList;
+    private ListView m_ItemsList;
 
     public List<CathegoryItemData> m_CathegoryItems;
 
@@ -12,7 +14,20 @@ public class CathegoriesController : MonoBehaviour
     {
         m_UIDocument = GetComponent<UIDocument>();
 
-        var listView = m_UIDocument.rootVisualElement.Q<ListView>("CathegoryList");
-        listView.itemsSource = m_CathegoryItems;
+        m_CathegoriesList = m_UIDocument.rootVisualElement.Q<ListView>("CathegoryList");
+        m_CathegoriesList.itemsSource = m_CathegoryItems;
+        m_CathegoriesList.selectionChanged += OnCathegorySelection;
+
+        m_ItemsList = m_UIDocument.rootVisualElement.Q<ListView>("ItemsList");
+        m_ItemsList.parent.style.display = DisplayStyle.None;
+    }
+
+    private void OnCathegorySelection(IEnumerable<object> enumerable)
+    {
+        if (enumerable != null)
+            m_ItemsList.parent.style.display = DisplayStyle.Flex;
+
+        foreach (CathegoryItemData item in enumerable)
+            Debug.Log($"Selected item id: {item.Id}");
     }
 }
