fc0e56530434ebb4486e32637af7278cc343422b
Aleksey Kuznetsov <oasisalex@gmail.com>
2025-05-29 01:21:20 +0300

    hover fixes

--- Assets/HoverDetector.cs
+++ Assets/HoverDetector.cs
@@ -1,36 +1,23 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class HoverDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler
+public class HoverDetector : MonoBehaviour
 {
-    private Vector2 m_CurrentPointerPos;
+    private InputAction m_PointerPositionAction;
+    private LayerMask m_LayerMask;
+    private Camera m_Camera;
 
     [SerializeField] private Material m_HoverMaterial = default;
     [SerializeField] private MeshRenderer[] m_Meshes = default;
     [SerializeField] private List<Material> m_DefaultMaterials = default;
 
-    private Camera m_Camera;
-
-    public bool IsCaptured
-    {
-        get
-        {
-            Ray ray = m_Camera.ScreenPointToRay(m_CurrentPointerPos);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                Debug.Log("Hits collider on object: " + hit.transform.name);
-                return hit.transform == transform;
-            }
-
-            return false;
-        }
-    }
-
     private void Awake()
     {
         m_Camera = Camera.main;
+        m_LayerMask = gameObject.layer;
 
         var meshCounter = 0;
 
@@ -39,35 +26,47 @@ public class HoverDetector : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             m_DefaultMaterials.Add(meshRenderer.material);
             meshCounter++;
         }
+
+        m_PointerPositionAction = InputSystem.actions.FindAction("DragPosition");
+        m_PointerPositionAction.Enable();
+        m_PointerPositionAction.performed += OnPointerMove;
+    }
+
+    private void OnPointerMove(InputAction.CallbackContext context)
+    {
+        Vector2 mousePosition = context.ReadValue<Vector2>();
+        Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(ray, out var hit, 10))
+        {
+            if (hit.transform == transform)
+            {
+                PointerEnter();
+                return;
+            }
+        }
+
+        PointerLeave();
     }
 
     private void OnDestroy()
     {
+        m_PointerPositionAction.Disable();
+        m_PointerPositionAction.performed -= OnPointerMove;
+
         var collider = GetComponentInChildren<Collider>();
         collider.enabled = false;
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    public void PointerEnter()
     {
-
         for (var i = 0; i < m_Meshes.Length; i++)
             m_Meshes[i].material = m_HoverMaterial;
-
-        Debug.Log("Hover on");
     }
 
-    public void OnPointerExit(PointerEventData eventData)
+    public void PointerLeave()
     {
         for (var i = 0; i < m_Meshes.Length; i++)
             m_Meshes[i].material = m_DefaultMaterials[i];
-
-        Debug.Log("Hover leave");
-    }
-
-    public void OnPointerMove(PointerEventData eventData)
-    {
-        m_CurrentPointerPos = eventData.position;
-        if (IsCaptured)
-            Debug.Log("Hover on");
     }
 }
